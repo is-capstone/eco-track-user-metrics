cf2bc1aa97b9277733419675b997cfe62e84699a
Vladislav Andreev <andreevvlad443@gmail.com>
2025-03-05 20:57:12 +0300

    chore: api endpoints refactor

--- src/main/java/com/enzulode/metrics/crud/api/MetricsValuesController.java
+++ src/main/java/com/enzulode/metrics/crud/api/MetricsValuesController.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/values")
+@RequestMapping("/metrics-values")
 @RequiredArgsConstructor
 public class MetricsValuesController {
 
