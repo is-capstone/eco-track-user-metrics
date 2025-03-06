9c8894c904a1c558bccdddc4739aa266865588fe
Vladislav Andreev <andreevvlad443@gmail.com>
2025-03-06 06:20:13 +0300

    fix: existing items detection on update

--- src/main/java/com/enzulode/metrics/crud/service/UnitsServiceImpl.java
+++ src/main/java/com/enzulode/metrics/crud/service/UnitsServiceImpl.java
@@ -2,9 +2,7 @@ package com.enzulode.metrics.crud.service;
 
 import com.enzulode.metrics.crud.dao.entity.Units;
 import com.enzulode.metrics.crud.dao.repository.UnitsRepository;
-import com.enzulode.metrics.crud.exception.ItemAlreadyExistsException;
 import com.enzulode.metrics.crud.exception.ItemNotFoundException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,16 +17,7 @@ public class UnitsServiceImpl extends AbstractBaseCrudService<Units, Long> imple
     var existingUnits = repository.findById(id)
         .orElseThrow(() -> new ItemNotFoundException("Item was not found"));
 
-    try {
-      existingUnits.setTitle(entity.getTitle());
-      return repository.save(existingUnits);
-    } catch (DataIntegrityViolationException e) {
-      var message = e.getMessage();
-
-      if (message.contains("violates unique constraint"))
-        throw new ItemAlreadyExistsException("Item already exists", e);
-
-      throw e;
-    }
+    existingUnits.setTitle(entity.getTitle());
+    return this.create(existingUnits);
   }
 }
